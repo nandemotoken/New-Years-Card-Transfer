29fd936818298c9cbaf6e27a5824d3e33d3d9343
nandemotoken <nandemotoken@gmail.com>
2020-12-31 18:48:58 +0000

    make files

--- assets/js/GetCardotukarehitoiki1.js
+++ assets/js/GetCardotukarehitoiki1.js
@@ -20,11 +20,16 @@ async function makecardotukarehitoiki1(){
   mycontract = await new web3tr.eth.Contract(abi, Address);
   let useraddress = await web3tr.eth.getAccounts();
   document.getElementById("idn").innerHTML = "年賀状発行には約20秒かかります"
-  setTimeout( () => { document.getElementById("idn").innerHTML = "年賀状を発行中です…" } , 5000);
-  setTimeout( () => { document.getElementById("idn").innerHTML = "年賀状を発行中です……" } , 10000)
+  setTimeout( () => { document.getElementById("idn").innerHTML = "年賀状を発行中です" } , 10000);
+  setTimeout( () => { document.getElementById("idn").innerHTML = "年賀状を発行中です…" } , 11000);
+  setTimeout( () => { document.getElementById("idn").innerHTML = "年賀状を発行中です……" } , 12000);
+  setTimeout( () => { document.getElementById("idn").innerHTML = "年賀状を発行中です………" } , 13000);
+  setTimeout( () => { document.getElementById("idn").innerHTML = "年賀状を発行中です…………" } , 14000);
+  setTimeout( () => { document.getElementById("idn").innerHTML = "年賀状を発行中です……………" } , 15000);
+  setTimeout( () => { document.getElementById("idn").innerHTML = "取得したNFT年賀状を確認ボタンを押してください" } , 30000);
   let ret = await mycontract.methods.mint().send({ from: useraddress[0] });
   //ret.on("receipt" , setTimeout( checkcard() , 5000 ));;
 	document.getElementById("idn").innerHTML = "年賀状到着まで約20秒お待ちください";
-  setTimeout( checkcardotukarehitoiki1() , 10000 )
+  setTimeout( checkcardotukarehitoiki1() , 15000 )
 	console.log("useraddress[0]_is_your_Address:" + useraddress[0]);
 }
--- assets/js/GetCardsalmon_crypto.js
+++ assets/js/GetCardsalmon_crypto.js
@@ -20,11 +20,20 @@ async function makecardsalmon_crypto(){
   mycontract = await new web3tr.eth.Contract(abi, Address);
   let useraddress = await web3tr.eth.getAccounts();
   document.getElementById("idn").innerHTML = "年賀状発行には約20秒かかります"
-  setTimeout( () => { document.getElementById("idn").innerHTML = "年賀状を発行中です…" } , 5000);
-  setTimeout( () => { document.getElementById("idn").innerHTML = "年賀状を発行中です……" } , 10000)
+  setTimeout( () => { document.getElementById("idn").innerHTML = "年賀状を発行中です" } , 1000);
+  setTimeout( () => { document.getElementById("idn").innerHTML = "年賀状を発行中です…" } , 2000);
+  setTimeout( () => { document.getElementById("idn").innerHTML = "年賀状を発行中です……" } , 3000);
+  setTimeout( () => { document.getElementById("idn").innerHTML = "年賀状を発行中です………" } , 4000);
+  setTimeout( () => { document.getElementById("idn").innerHTML = "年賀状を発行中です…………" } , 5000);
+  setTimeout( () => { document.getElementById("idn").innerHTML = "年賀状を発行中です……………" } , 6000);
+  setTimeout( () => { document.getElementById("idn").innerHTML = "年賀状を発行中です………………" } , 7000);
+  setTimeout( () => { document.getElementById("idn").innerHTML = "年賀状を発行中です…………………" } , 8000);
+  setTimeout( () => { document.getElementById("idn").innerHTML = "年賀状を発行中です……………………" } , 9000);
+  setTimeout( () => { document.getElementById("idn").innerHTML = "年賀状を発行中です………………………" } , 10000);
+  setTimeout( () => { document.getElementById("idn").innerHTML = "取得したNFT年賀状を確認ボタンを押してください" } , 30000);
   let ret = await mycontract.methods.mint().send({ from: useraddress[0] });
   //ret.on("receipt" , setTimeout( checkcard() , 5000 ));;
-	document.getElementById("idn").innerHTML = "年賀状到着まで約20秒お待ちください";
-  setTimeout( checkcardsalmon_crypto() , 10000 )
+  document.getElementById("idn").innerHTML = "年賀状到着まで約20秒お待ちください";
+  setTimeout( checkcardsalmon_crypto() , 15000 )
 	console.log("useraddress[0]_is_your_Address:" + useraddress[0]);
 }
